6f4d99f22a374d0cf33ecf9eb61885e2c0fc32e3
Noa <noaminha.student@gmail.com>
2025-04-02 19:49:26 +0200

    added tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	let name = 'Scuttle';
+    let img = '/Profile_-_Scuttle.png'
+</script>
+
+<div class="bg-blue-500 text-white text-2xl p-4 rounded-lg">
+    🚀 Tailwind CSS is working!
+  </div>
+
+<div>
+    <h1 class="font-sans color-red">Good Morning {name}!</h1>
+
+    <img src={img} alt="Your profile pic" />
+</div>
+
